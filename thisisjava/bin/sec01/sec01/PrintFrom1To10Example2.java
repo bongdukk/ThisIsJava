b012522ff230f934a4ee6229b7ade0e0ad3eefa2
bongdukk <128344976+bongdukk@users.noreply.github.com>
2023-06-07 20:56:20 +0900

    Add files via upload

---
+++ thisisjava/bin/sec01/sec01/PrintFrom1To10Example2.java
@@ -0,0 +1,11 @@
+package ch04.sec01;
+
+public class PrintFrom1To10Example2 {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i<=10) {
+            System.out.print(i + " ");
+            i++;
+        }
+    }
+}
